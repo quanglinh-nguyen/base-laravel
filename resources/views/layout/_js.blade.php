7d3244f606879e0f2097b5775d346b3feed9618e
TIENrikai123 <qtien.nguyen@rikai.technology>
2022-12-15 17:37:04 +0700

    Design Sidebar and Header page admin

---
+++ resources/views/layout/_js.blade.php
@@ -0,0 +1,6 @@
+<!-- jQuery -->
+<script src="{{asset('template/plugins/jquery/jquery.min.js')}}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{asset('template/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('template/dist/js/adminlte.js')}}"></script>
