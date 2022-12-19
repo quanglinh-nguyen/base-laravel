b178dae84eb4b8db5ef32025ed4bb11a2946acec
Huu Quang Linh Nguyen <quanglinh.nguyen@rikai.technology>
2022-12-19 17:47:41 +0700

    fix notifications

--- resources/views/common/notifications.blade.php
+++ resources/views/common/notifications.blade.php
@@ -17,6 +17,6 @@
         "hideMethod": "fadeOut"
     }
     @if(session()->has('success')) Command: toastr["success"]("{!! nl2br(htmlentities(session()->get('success'))) !!}", "Success" ); @endif
-        @if(session()->has('success')) Command: toastr["warning"]("{!! nl2br(htmlentities(session()->get('warning'))) !!}", "Warning"); @endif
-        @if(session()->has('success')) Command: toastr["error"]("{!! nl2br(htmlentities(session()->get('error'))) !!}", "Error"); @endif
+    @if(session()->has('warning')) Command: toastr["warning"]("{!! nl2br(htmlentities(session()->get('warning'))) !!}", "Warning"); @endif
+    @if(session()->has('error')) Command: toastr["error"]("{!! nl2br(htmlentities(session()->get('error'))) !!}", "Error"); @endif
 </script>
