d7b27f8eaad9d8fb73b0e7daf8e39760241ba659
TIENrikai123 <qtien.nguyen@rikai.technology>
2022-12-21 14:52:46 +0700

    fix header: add dropdown to header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -13,43 +13,19 @@
       </li>
     </ul>
 
-    <!-- Right navbar links -->
-    <!-- Profile Dropdown Menu -->
     <ul class="navbar-nav ml-auto">
-      <!-- Notifications Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-bell"></i>
-          <span class="badge badge-warning navbar-badge">15</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-item dropdown-header">15 Notifications</span>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-envelope mr-2"></i> 4 new messages
-            <span class="float-right text-muted text-sm">3 mins</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-users mr-2"></i> 8 friend requests
-            <span class="float-right text-muted text-sm">12 hours</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-file mr-2"></i> 3 new reports
-            <span class="float-right text-muted text-sm">2 days</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+      <li class="nav-item dropdown d-flex flex-row justify-content-end align-items-center">
+        <div class="text-center" style="width:50px">
+          <img src="{{asset('template/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image" width="40px">
         </div>
-      </li>
-      
-      <li class="nav-item dropdown d-flex flex-row">
-          <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle shadow-sm">Nguyễn Quyết Tiến</a>
+        <div class="px-2" style="position: relative">
+          <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle px-0">Nguyễn Hữu Quang Linh</a>
           <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0">
             <li class="dropdown-li"><a href="{{ route('home.profile')}}" class="dropdown-item">Profile</a></li>
             <li class="dropdown-li"><a href="#" class="dropdown-item">Logout</a></li>
           </ul>
+        </div>
+        
       </li>
     </ul>
   </nav>
