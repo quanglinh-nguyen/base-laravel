79a333e4308f15dc8f3c7c3583d201239b2b04ce
TIENrikai123 <qtien.nguyen@rikai.technology>
2022-12-23 17:56:51 +0700

    fix sidebar and login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,101 +3,70 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Log in (v2)</title>
-
+  <title>IEC Group</title>
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{asset('template/plugins/fontawesome-free/css/all.min.css')}}">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="{{asset('template/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset('template/dist/css/adminlte.min.css')}}">
+  <link rel="stylesheet" href="{{asset('template/dist/css/adminlte.css')}}">
 </head>
-{{-- <style>
-    [class*=icheck-]>input:first-child+input[type=hidden]+label::before, [class*=icheck-]>input:first-child+label::before {
-    content: "";
-    display: inline-block;
-    position: absolute;
-    width: 22px;
-    height: 22px;
-    border: 1px solid #D3CFC8;
-    border-radius: 0;
-    margin-left: -29px;
-}
-</style> --}}
-<body class="hold-transition login-page">
-<div class="login-box">
-  <!-- /.login-logo -->
-  <div class="card card-outline card-primary">
-    <div class="card-header text-center">
-      <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+<style>
+  section{
+    background-image: linear-gradient(to right, rgba(17, 100, 102, 1) 0%, rgba(4, 29, 30, 1) 100%);
+  }
+  .btn-block{
+    background-color: #116466;
+    box-shadow: 0 4px 9px -4px #332d2d;
+    border-color: #116466
+  }
+  .btn-block:hover{
+    background-color: #0e5e5f;
+    box-shadow: 0 5px 9px -4px #332d2d;
+    border-color: #116466
+  }
+</style>
+<body>
 
-      <form action="../../index3.html" method="post">
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+<section class="vh-100" style="">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-xl-10 ">
+        <div class="card" style="border-radius: 1rem;">
+          <div class="row">
+            <div class="col-md-6 col-lg-5 d-none d-md-block">
+              <img src="{{asset('template/dist/img/Banner-login.jpg')}}"
+                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;object-fit: cover; max-width: 100%;"/>
+            </div>
+            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+              <div class="card-body px-lg-5 pb-lg-5 text-black">
+                <form>
+                  <div class="d-flex align-items-center mb-2">
+                    <img src="{{asset('template/dist/img/Logo-IEC-2020_white_250.png')}}" alt="" width="19%"> 
+                  </div>
+                  <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
+                  <div class="form-outline mb-3">
+                    <label class="form-label" for="form2Example17">Email address</label>
+                    <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                    <label id="inputAcronyms-error" class="error" for="inputAcronyms"></label>
+                  </div>
+                  <div class="form-outline mb-3">
+                    <label class="form-label" for="form2Example27">Password</label>
+                    <input type="password" id="form2Example27" class="form-control form-control-lg"/>
+                    <label id="inputAcronyms-error" class="error" for="inputAcronyms"></label>
+                  </div>
+                  <div class="pt-1 mb-4">
+                    <button class="btn btn-lg btn-block text-white" type="button">Login</button>
+                  </div>
+                  <a class="text-muted" href="#">Forgot password?</a>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember" style="padding:2pxpx;">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-
-      <div class="social-auth-links text-center mt-2 mb-3">
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
       </div>
-      <!-- /.social-auth-links -->
-
-      <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p>
-      <p class="mb-0">
-        <a href="register.html" class="text-center">Register a new membership</a>
-      </p>
     </div>
-    <!-- /.card-body -->
   </div>
-  <!-- /.card -->
-</div>
-<!-- /.login-box -->
-
-<!-- jQuery -->
-<script src="{{asset('template/plugins/jquery/jquery.min.js')}}"></script>
-<!-- Bootstrap 4 -->
-<script src="{{asset('template/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<!-- AdminLTE App -->
-<script src="{{asset('template/dist/js/adminlte.min.js')}}"></script>
+</section>
 </body>
 </html>
