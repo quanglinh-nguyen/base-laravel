e71c6d045f7ef0dd43144c1fb07e9f65df0805f1
Huu Quang Linh Nguyen <quanglinh.nguyen@rikai.technology>
2022-12-27 10:16:56 +0700

    fix validate

--- public/view/acronyms-banking/form-validation.js
+++ public/view/acronyms-banking/form-validation.js
@@ -1,13 +1,27 @@
-$().ready(function() {
-    $("#create-update-acronyme-banking").validate({
-        rules: {
-            acronym: {
-                required: true,
-            },
-            full_name: {
-                required: true,
-            },
+jQuery.validator.addMethod(
+    "checkForSpecialChar",
+    function (value, element) {
+        let format = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+        if(format.test(value)){
+            return false;
+        } else {
+            return true;
+        }
+    },
+"This field is not contain special characters"
+);
+
+$("#create-update-acronyme-banking").validate({
+    rules: {
+        acronym: {
+            required: true,
+            checkForSpecialChar: true
         },
-    });
-})
+        full_name: {
+            required: true,
+            checkForSpecialChar: true
+        },
+    },
+});
+
 
