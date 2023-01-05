f2e1234a277c748a8af5d4557609dbd124cb8cb3
quang Linh <quanglinh.nguyen@rikai.technology>
2023-01-06 00:19:58 +0700

    add model and policy

---
+++ app/Policies/CustomersErrorPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\CustomersError;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CustomersErrorPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\CustomersError  $customersError
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, CustomersError $customersError)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\CustomersError  $customersError
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, CustomersError $customersError)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\CustomersError  $customersError
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, CustomersError $customersError)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\CustomersError  $customersError
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, CustomersError $customersError)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\CustomersError  $customersError
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, CustomersError $customersError)
+    {
+        //
+    }
+}
